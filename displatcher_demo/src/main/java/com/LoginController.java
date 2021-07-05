b82ee0767dfecf8e435d65329814880fe059bea8
Talal <talal6662012@hotmail.com>
2021-07-05 16:26:45 +0300

    First commit

---
+++ displatcher_demo/src/main/java/com/LoginController.java
@@ -0,0 +1,50 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		RequestDispatcher rd = null;
+		if(username.equalsIgnoreCase("talal") && password.equals("Dulaym")) {
+			rd = request.getRequestDispatcher("SuccessServlet");
+			rd.forward(request, response);
+		}else {
+			rd = request.getRequestDispatcher("login.html");
+			PrintWriter out = response.getWriter();
+			rd.include(request, response);
+			out.println("<center> <span style='color:red'>Invalid Credentials!</span></center>");
+			
+		}
+		
+		
+	}
+
+}
